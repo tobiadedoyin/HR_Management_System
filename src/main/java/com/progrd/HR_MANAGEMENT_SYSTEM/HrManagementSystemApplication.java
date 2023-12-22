91222fc9c7ef4e2cfc18015092a447cbb5fb0d7e
Tobi Oladele <oladeletobiadedoyin@gmail.com>
2023-12-22 23:12:03 +0100

    setup

---
+++ src/main/java/com/progrd/HR_MANAGEMENT_SYSTEM/HrManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.progrd.HR_MANAGEMENT_SYSTEM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HrManagementSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HrManagementSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/progrd/HR_MANAGEMENT_SYSTEM/HrManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.progrd.HR_MANAGEMENT_SYSTEM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HrManagementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
