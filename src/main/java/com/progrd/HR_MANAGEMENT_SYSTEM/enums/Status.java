6f9249e96da2350035190d55deb10b1005a08d13
Mary Adewunmi <physayoemma@gmail.com>
2023-12-27 21:47:15 +0100

    leave service modified

---
+++ src/main/java/com/progrd/HR_MANAGEMENT_SYSTEM/enums/Status.java
@@ -0,0 +1,5 @@
+package com.progrd.HR_MANAGEMENT_SYSTEM.enums;
+
+public enum Status {
+    Approved, Pending, Rejected
+}
---
+++ src/main/java/com/progrd/HR_MANAGEMENT_SYSTEM/model/leave.java
@@ -0,0 +1,33 @@
+package com.progrd.HR_MANAGEMENT_SYSTEM.model;
+
+import com.progrd.HR_MANAGEMENT_SYSTEM.enums.LeaveType;
+import com.progrd.HR_MANAGEMENT_SYSTEM.enums.Status;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@Entity
+@AllArgsConstructor
+@RequiredArgsConstructor
+@Table(name = "leave")
+public class leave {
+
+    @Id
+    @GeneratedValue
+    private Integer leaveId;
+
+    private Date startDate;
+
+    private Date endDate;
+
+    @Enumerated
+    private LeaveType leaveType;
+
+    @Enumerated
+    private Status status;
+
+}
