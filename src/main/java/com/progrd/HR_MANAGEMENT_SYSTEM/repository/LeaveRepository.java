59b53e7d408dc29f4550334049f97e3ccebab5d5
Mary Adewunmi <physayoemma@gmail.com>
2023-12-29 20:53:33 +0100

    implemented leave controller and service

--- src/main/java/com/progrd/HR_MANAGEMENT_SYSTEM/repository/LeaveRepository.java
+++ src/main/java/com/progrd/HR_MANAGEMENT_SYSTEM/repository/LeaveRepository.java
@@ -1,7 +1,7 @@
 package com.progrd.HR_MANAGEMENT_SYSTEM.repository;
 
-import com.progrd.HR_MANAGEMENT_SYSTEM.entity.leave;
+import com.progrd.HR_MANAGEMENT_SYSTEM.entity.Leave;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface LeaveRepository extends JpaRepository<leave,Integer> {
+public interface LeaveRepository extends JpaRepository<Leave,Integer> {
 }
