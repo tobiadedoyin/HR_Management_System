8166a44f2d47d2dbc547c00e8abb1aa32de4b375
Kayode Alegbemi <kayode.alegbemi@gmail.com>
2024-01-06 22:03:24 +0100

    implementing the logic to send mail to hr for leave review and also send mail to notify employee of a pending leave review

--- src/main/java/com/progrd/HR_MANAGEMENT_SYSTEM/util/MailMessageUtil.java
+++ src/main/java/com/progrd/HR_MANAGEMENT_SYSTEM/util/MailMessageUtil.java
@@ -12,7 +12,20 @@ public class MailMessageUtil {
     private  final LeaveRepository leaveRepository;
     //private final LeaveDto leaveDto;
 
-    public String getPendingMessage(String name, String host, int Lid){
+    public String getPendingMessageForReview(String name, String host, int Lid){
+        Leave leave1 =leaveRepository.findById(Lid).get();
+        LeaveDto leave_detail = new LeaveDto();
+        leave_detail.setLeaveType(leave1.getLeaveType());
+        leave_detail.setStartDate(leave1.getStartDate());
+        leave_detail.setEmployeeId(leave1.getEmployees().getId());
+        leave_detail.setEndDate(leave1.getEndDate());
+
+        return ("Dear "+name+"\n\nyour leave applications as seen below is awaiting review\n\n"+leave_detail
+                + "\n\nuse this link to approve "+getApprovalLink(host, Lid)+"\nOR\nthis link to reject and delete "
+                +getRejectionLink(host, Lid)+"\n\n\nThank you.");
+    }
+
+    public String getPendingMessage(String name, int Lid){
         Leave leave =leaveRepository.findById(Lid).get();
         LeaveDto leaveDto = new LeaveDto();
         leaveDto.setLeaveType(leave.getLeaveType());
@@ -21,9 +34,9 @@ public class MailMessageUtil {
         leaveDto.setEndDate(leave.getEndDate());
 
         return ("Dear "+name+"\n\nyour leave applications as seen below is awaiting review\n\n"+leaveDto
-                + "\n\nuse this link to approve "+getApprovalLink(host, Lid)+"\nOR\nthis link to reject and delete "
-                +getRejectionLink(host, Lid)+"\n\n\nThank you.");
+                +"\n\n\nThank you.");
     }
+
 
     private static String getApprovalLink(String host, int Lid) {
         return (host+"/api/leave/"+Lid);
